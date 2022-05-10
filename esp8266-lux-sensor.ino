8715b5efb3c03ba8a75f8da49a1cbde77d521478
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2022-05-10 14:16:23 +0100

    Seperate functions and add smoothing to sensor output

--- esp8266-lux-sensor.ino
+++ esp8266-lux-sensor.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <PubSubClient.h>
 #include <DFRobot_B_LUX_V30B.h>
+#include <ESP8266WebServer.h>
 
 DFRobot_B_LUX_V30B    myLux(13);//The sensor chip is set to 13 pins, SCL and SDA adopt default configuration
 // Update these with values suitable for your network.
@@ -13,10 +14,21 @@ const char* mqtt_clientId = "CHANGEME"; //put your MQTT Client name here.
 const char* mqtt_userName = "CHANGEME"; //put your MQTT Username here.
 const char* mqtt_passWord = "CHANGEME"; //put your MQTT Password here.
 
-
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsgSent = 0;
+
+const int numReadings = 10;
+
+int readings[numReadings];
+int readIndex = 0;
+int total = 0;
+int average = 0;
+
+//ESP8266WiFiMulti wifiMulti;
+ESP8266WebServer server(80);
+void handleRoot();
+void handleNotFound();
 
 void setup_wifi() {
   delay(100);
@@ -65,31 +77,63 @@ void reconnect() {
   }
 } //end reconnect()
 
-void setup() {
+void setup(void) {
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   myLux.begin();
+  server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
 }
 
-void loop() {
-  long now = millis();
+void handleRoot() {
+  server.send(200, "text/plain", "Hello world!");   // Send HTTP status 200 (Ok) and send some text to the browser/client
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
+
+void mqttMessage() {
+   long now = millis();
   //send message every minuite
-  delay(60000);
+  //delay(60000);
   lastMsgSent = now;
   static char mqttMsg[15];
 
-  float lux = myLux.lightStrengthLux();
-  dtostrf(lux, 6, 2, mqttMsg);
+//  float lux = myLux.lightStrengthLux();
+//  dtostrf(lux, 6, 2, mqttMsg);
 
+  total = total - readings[readIndex];
+  readings[readIndex] = myLux.lightStrengthLux();
+  total = total + readings[readIndex];
+  readIndex = readIndex + 1;
+
+  if (readIndex >= numReadings) {
+    readIndex = 0;
+  }
+
+  average = total / numReadings;
+  dtostrf(average, 6, 2, mqttMsg);
   Serial.println(mqttMsg);
+  delay(1000);
+  
+//  Serial.println(mqttMsg);
   if (client.connected() == false) {
     reconnect();
   }
   client.loop();
   //publish sensor data to MQTT broker
   client.publish("cmnd/lux-sensor/Value", mqttMsg);
-
 }
 
+void loop(void) {
+  server.handleClient();
+  mqttMessage();
+}
